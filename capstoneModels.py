689ca76ef7fde279c519ba6d2e7064b52f25877d
sreiter1 <sean.m.reiter1@gmail.com>
2022-06-12 13:51:44 -0600

    update

--- capstoneModels.py
+++ capstoneModels.py
@@ -1417,7 +1417,7 @@ if __name__ == "__main__":
     # mod.LSTM_load(modelToLoad="D:\\UCSD ML Repositories\\Capstone\\Model\\static\\LSTMmodels\\2022-06-12 13.20.07\\lstm_model_010.h5")
     
     x = mod.LSTM_train(EpochsPerTicker = 1, 
-                        fullItterations = 10, 
+                        fullItterations = 30, 
                         loadPrevious = False,
                         look_back = 120, 
                         trainSize = 0.9,
