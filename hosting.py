6519e54f9e671190968361a20897f890a803a0ac
sreiter1 <sean.m.reiter1@gmail.com>
2022-06-19 21:42:44 -0600

    update

--- hosting.py
+++ hosting.py
@@ -601,7 +601,7 @@ def query():
 
 if __name__ == '__main__':
     
-    app.run(debug=False, threaded=False, host='0.0.0.0', port=8080)
+    app.run(debug=False, host='0.0.0.0', port=8080)
         
     # if "win" in sys.platform:
     #     app.run(debug=True)
